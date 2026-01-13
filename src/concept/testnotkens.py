adcd94fda7ce988fb4bec1d15f2356881d5cef4f
IacopoDiPippo <dipippo.1966046@studenti.uniroma1.it>
2026-01-13 14:35:56 +0100

    Update testnotkens.py

--- src/concept/testnotkens.py
+++ src/concept/testnotkens.py
@@ -2,7 +2,7 @@ import anndata as ad
 import pandas as pd
 
 adata = ad.read_h5ad("/p/project1/hai_fzj_bda/spitzer2/point_transformer/data/raw/abc_atlas.h5ad")
-gene_mapping = pd.read_pickle(/p/scratch/cjinm16/dipippo1/scConcept/token_mapping/pc_gene_token_mapping.pkl).to_dict()
+gene_mapping = pd.read_pickle("/p/scratch/cjinm16/dipippo1/scConcept/token_mapping/pc_gene_token_mapping.pkl").to_dict()
 
 common = set(adata.var_names).intersection(set(gene_mapping.keys()))
 print("adata genes:", adata.n_vars)
