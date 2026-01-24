b69fc5faf4ba39093432b8bf02d3eec374bf32ee
IacopoDiPippo <dipippo.1966046@studenti.uniroma1.it>
2026-01-24 17:37:14 +0100

    Update train.py

--- src/concept/train.py
+++ src/concept/train.py
@@ -176,6 +176,7 @@ def train(cfg: DictConfig):
                         strategy=DDPStrategy(find_unused_parameters=True),
                         precision='bf16-mixed', 
                         use_distributed_sampler=False,
+                        num_sanity_val_steps=0
                         )
 
 
