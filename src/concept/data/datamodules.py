ad04d482d4b7443898e8db40f442a31e82f73254
IacopoDiPippo <dipippo.1966046@studenti.uniroma1.it>
2026-01-25 23:41:12 +0100

    Update datamodules.py

--- src/concept/data/datamodules.py
+++ src/concept/data/datamodules.py
@@ -92,7 +92,7 @@ class AnnDataModule(L.LightningDataModule):
                     keys_to_cache=keys_to_cache,
                     join=join,
                     encode_labels=True,
-                    parallel=False,
+                    parallel=True,
                     obsm_keys=precomp_embs_key
                 )
 
