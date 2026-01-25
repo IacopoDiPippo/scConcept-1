da385341669c892f7443263092a38d3dfaa1c8c1
IacopoDiPippo <dipippo.1966046@studenti.uniroma1.it>
2026-01-25 12:12:10 +0100

    s

--- src/concept/train.py
+++ src/concept/train.py
@@ -171,26 +171,12 @@ def train(cfg: DictConfig):
     import logging
     logging.getLogger("lightning.pytorch").setLevel(logging.DEBUG)
     logging.getLogger("lightning.fabric").setLevel(logging.DEBUG)
-
-    profiler = PyTorchProfiler(
-        dirpath=CHECKPOINT_PATH,          # o una cartella tipo "./prof"
-        filename="pytorch_profiler",      # crea file .txt e trace
-        export_to_chrome=True,            # ti crea trace.json
-        record_functions=True,
-        profile_memory=True,
-        with_stack=False,
-        with_flops=True,
-    )
-    trainer_kwargs["limit_train_batches"] = 50
-    trainer_kwargs["limit_val_batches"] = 10
-    trainer_kwargs["max_epochs"] = 1
-
+    
     trainer = L.Trainer(**trainer_kwargs, 
                         strategy=DDPStrategy(find_unused_parameters=True),
                         precision='bf16-mixed', 
                         use_distributed_sampler=False,
-                        num_sanity_val_steps=0,
-                        profiler=profiler,
+                        num_sanity_val_steps=0
                         )
 
 
