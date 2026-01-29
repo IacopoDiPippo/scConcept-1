3a1384eca37d0f20706ebfb2db82c8242fb200a8
IacopoDiPippo <dipippo.1966046@studenti.uniroma1.it>
2026-01-29 13:04:14 +0100

    Update train.py

--- src/concept/train.py
+++ src/concept/train.py
@@ -143,6 +143,7 @@ def train(cfg: DictConfig):
                         use_distributed_sampler=False,
                         num_sanity_val_steps=0,
                         limit_val_batches=0,
+                        max_epochs = 2
                         )
 
 
